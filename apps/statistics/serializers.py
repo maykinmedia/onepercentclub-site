b0a1918c256c0a69c3933956a22429eae123a461
Joeri Bekker <joeri@maykinmedia.nl>
2013-09-24 12:23:29 +0200

    Cleaned the project from all apps and project settings except for the apps accounts, bb_utils, bb_drf2 and geo which are moved to the bluebottle namespace. This in an initial attempt to make them generic on a Django and Python package level.

--- apps/statistics/serializers.py
+++
@@ -1,9 +0,0 @@
-from apps.accounts.serializers import UserPreviewSerializer
-from rest_framework import serializers
-from .models import Statistic
-
-
-class StatisticSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Statistic
